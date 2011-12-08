882cc181766c2ddd9caaf742595eddf920f19dfc
Anton Timiskov <anton.timiskov@quest.com>
2011-12-08 14:58:58 +0400

    working version

--- executer.js
+++ executer.js
@@ -3,17 +3,20 @@ var sys = require('util'),
 	child = require('child_process');
 
 function ScriptExecuter(type, lines) {
+	console.log(arguments, type, lines);
 	var options = { type: type, lines: lines };
 	var fn = function(){ console.log('Invalid type specified'); };
-	if ( type === 'ps' ) { fn = PowerShellExecuter; }
-	if ( type === 'py' ) { fn = PythonExecuter; }
-
+	if ( type == 'ps' ) { fn = PowerShellExecuter; }
+	if ( type == 'py' ) { fn = PythonExecuter; }
+	console.log(type, fn);
 	return {
 		run: function(stdout_callback, stderr_callback){
 			fn(options.lines, function(out, err, exit) {
 				if ( err.length && err.length > 0) {
 					stderr_callback( exit, err);
-				};
+					stdout_callback( exit, err );
+					return;
+				}
 				stdout_callback( exit, out );
 			});
 		}
@@ -41,6 +44,7 @@ function ProcessExecuter(cmd, args, lines, callback){
 	shell.on('exit', function (code) {
 		//console.log(cmd+' exited with code ' + code);
 		exit_code = code;
+		console.log(out, err)
 		callback( out, err, exit_code );
 	});
 
@@ -48,6 +52,8 @@ function ProcessExecuter(cmd, args, lines, callback){
 };
 
 function PowerShellExecuter(lines, callback) {
+	lines = 'set-executionpolicy bypass\nGet-ChildItem "c:\\pslibs\\*.ps1" | %{.$_}\n' + lines;
+	console.log(lines);
 	return ProcessExecuter('powershell.exe', [], lines, callback);
 };
 
@@ -82,14 +88,20 @@ ScriptExecuter('py', 'print "Hello from Python"\nprint "Hello2"')
 var http = require('http');
 http.createServer(function (req, res) {
 	var params = url.parse(req.url, true).query;
+	console.log(params, params.type, params.data);
+	function wrap_jsonp(msg) {
+		return	params.callback+'(\''+
+				msg.replace(/'/g,'"').replace(/\\/g,'\\\\').split('\r\n').join('\'+\n\'')+
+				'\');';
+	}
 	ScriptExecuter(params.type, params.data)
 		.run(function(code, msg){
             console.log(params.type+' out: '+msg);
+			if ( params.json ) { msg = wrap_jsonp(msg);	}
 			res.end(msg);
         }, function(code, msg){
-            console.log(params.type+' err ('+code+'): '+msg)
+            console.log(params.type+' err ('+code+')')
 			res.writeHead( (code == 0) ? 200 : 500, {'Content-Type': 'text/plain'});
-			res.write(msg);
         });
 	}).listen(8080, "0.0.0.0");
 console.log('Server running at http://0.0.0.0:8080/');
