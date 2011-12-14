5f9513006b4c1620351bedadba5f94a4bd1788a4
Anton Timiskov <anton.timiskov@quest.com>
2011-12-14 14:34:42 +0400

    new realization

--- ps.js
+++ ps.js
@@ -1,47 +1,62 @@
-function PS(){
+function PS(reject_on_fail){
   var thisPS = this;
   this.PS = this;
+  this.reject_on_fail = reject_on_fail;
   var server_url = 'http://'+context.host+':'+context.server_instance_port+'/';
   this.deferreds = [];
   
   function _invoke(data){
     console.log('do invoke', data);
     var dfd = new $.Deferred();
+    var random = Math.floor( Math.random() * 1000000000 ).toString();
+    var errorCallbackName = 'error' + random;
+    var successCallbackName = 'success' + random;
+    
+    window[errorCallbackName]  = function(out){
+      QUnit.log('PS ERROR: ', out);
+      if ( reject_on_fail ) {
+        dfd.reject();
+      } else {
+        dfd.resolve();
+      }
+    };
+    window[successCallbackName]  = function(out){
+      console.log('success', arguments);
+      QUnit.log('PS OK', out);
+      dfd.resolve();
+    };
+    
     $.ajax({
       url: server_url,
       dataType: 'jsonp',
       cache: false,
       async: true,
       method: 'GET',
-      data: { 
-        type: 'ps', 
-        data: data, 
-        json: true, 
-        path: 'C:\\SAIP\\SharePoint Information Portal\\Application\\tests\\cases\\libraries' 
+      data: {
+        type: 'ps',
+        data: data,
+        json: true,
+        callback: successCallbackName,
+        error: errorCallbackName,
+        path: 'C:\\SAIP\\SharePoint Information Portal\\Application\\tests\\cases\\libraries'
       },
-      success: function(out) {
-        $('#section1 .output').html(out.replace('/\r/g', '<br/>'));
-        console.log('out', out);
-        dfd.resolve();
-      }, 
-      error: function(a,b,error){
-        var out = error.message;
-        $('#section1 .output').html(out.replace('/\r/g', '<br/>'));
-        console.log('err', out);
-        dfd.reject();
+      complete: function(){
+        console.log(arguments);
       }
     });
     return dfd.promise();
   }
+  
+  this.failCallback = function(){};
   this.invoke = function(method, arg1, arg2, arg3){
     console.log(this, 'invoke', arguments);
     var dfd = new $.Deferred();
     var args = Array.prototype.slice.call(arguments).slice(1);
-    var data =  method + '('+ $.map(args, function(i){ 
-      if ( typeof i == 'string' ) return '"'+i+'"'; 
-      if ( typeof i == 'boolean' ) return '$'+i; 
-      return i; 
-    }).join(',') + ')';
+    var data =  method + ' '+ $.map(args, function(i){
+      if ( typeof i == 'string' ) return '"'+i+'"';
+      if ( typeof i == 'boolean' ) return '$'+i;
+      return i;
+    }).join(' ');
     console.log(data, arguments, args);
     this.deferreds.push(function() { return _invoke(data);});
     return this;
@@ -49,22 +64,34 @@ function PS(){
   this.then = function(callback){
     console.log(this, 'then', arguments);
     var busy = false,
-        funcs = this.deferreds;
+        funcs = this.deferreds,
+        gotError = false;
     
     (function loop(){
       if ( funcs.length == 0 ) {
-        callback();
-        return; 
+        if ( ! gotError ) callback();
+        return;
           }
       if ( ! busy ) {
         var fn = funcs.shift();
         busy = true;
-        fn().then(function(){
+        fn().done(function(){
           busy = false;
-        });
+        }).fail(function(){
+          funcs = [];
+          busy = false;
+          gotError = true;
+          thisPS.failCallback();
+        });;
       }
       setTimeout(loop, 100);
     })();
+    return this;
+  };
+  this.fail = function(callback){
+    this.failCallback = callback;
+    reject_on_fail = true;
+    return this;
   };
 }
 
